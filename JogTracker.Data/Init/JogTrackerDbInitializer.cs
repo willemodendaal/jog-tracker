7875ccd978ffe190a20a6d4db2894555727fbb9e
willem <willem@newmedialabs.co.za>
2015-06-25 21:04:00 +0200

    Added Unity webApi bootstrap.

--- JogTracker.Data/Init/JogTrackerDbInitializer.cs
+++ JogTracker.Data/Init/JogTrackerDbInitializer.cs
@@ -16,8 +16,8 @@ namespace JogTracker.Data.Init
         protected override void Seed(JogDbContext context)
         {
             context.JogEntries.AddRange(new[] {
-                new JogEntry() { DistanceKM = 3, StartDateTime = DateTime.Now.AddMonths(-2), Duration = new TimeSpan(0,30,2) },
-                new JogEntry() { DistanceKM = 3.5, StartDateTime = DateTime.Now.AddMonths(-1),  Duration = new TimeSpan(0,26,18)  },
+                new JogEntry() { DistanceKM = 3F, DateTime = DateTime.Now.AddMonths(-2), Duration = new TimeSpan(0,30,2) },
+                new JogEntry() { DistanceKM = 3.5F, DateTime = DateTime.Now.AddMonths(-1),  Duration = new TimeSpan(0,26,18)  },
             });
 
             context.SaveChanges();
