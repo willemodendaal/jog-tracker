43bf603de76d1bfba7b6f30a136df1e5e5508c4b
willem <willem@newmedialabs.co.za>
2015-06-27 12:05:48 +0200

    Added default super user to seed method.

--- JogTracker.Data/Migrations/Configuration.cs
+++ JogTracker.Data/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 namespace JogTracker.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -12,9 +14,33 @@ namespace JogTracker.Data.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(JogTracker.Data.JogDbContext context)
+        protected override void Seed(JogDbContext context)
         {
             // Logic here to seed with initial data.
+            AddDefaultAdminUser(context);
+        }
+
+        bool AddDefaultAdminUser(JogDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            IdentityResult identityResult = roleManager.Create(new IdentityRole("administrator"));
+            var userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(context));
+
+            var user = new IdentityUser()
+            {
+                UserName = "jogSuperUser",
+            };
+
+            //TODO: store password more securely.
+            identityResult = userManager.Create(user, "runUpYonderHills!");
+
+            if (identityResult.Succeeded == false)
+            {
+                return false;
+            }
+
+            identityResult = userManager.AddToRole(user.Id, "administrator");
+            return identityResult.Succeeded;
         }
     }
 }
