16ac0dc1433b27bc830c62f98e19123c2a8acff2
willem <willem@newmedialabs.co.za>
2015-06-28 14:08:58 +0200

    Added placeholder JogEntry api tests.

--- JogTracker.TestApi/TestJogEntryCrud.cs
+++ JogTracker.TestApi/TestJogEntryCrud.cs
@@ -4,11 +4,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace JogTracker.TestApi
 {
     [TestClass]
-    public class TestJogEntryCrud
+    public class TestJogEntryCrud : TestBase
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCreateEntries()
         {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestEditEntry()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestDeleteEntry()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestListEntries()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestListEntries_AsAdmin()
+        {
+            //Ensure admin can list other users jog entries.
+            Assert.Fail();
         }
     }
 }
