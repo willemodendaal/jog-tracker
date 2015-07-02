fe00486433f33422dbd90d2781a241be4a7a1e1a
Willem Odendaal <willem@newmedialabs.co.za>
2015-07-02 20:21:24 +0200

    Login handler added to mainController.js

--- SPAClient/src/js/controllers/mainController.js
+++ SPAClient/src/js/controllers/mainController.js
@@ -4,19 +4,28 @@
         .module('jogTracker')
         .controller('mainController', mainController);
 
-    mainController.$inject = ['$scope', '$log', 'jogDataFactory', 'accountFactory'];
+    mainController.$inject = [
+        '$scope',
+        '$log',
+        '$location',
+        'accountFactory'];
 
-    function mainController($scope, $log, jogDataFactory, accountFactory) {
+    function mainController($scope, $log, $location, accountFactory) {
         $scope.registerPlease = function () {
             var p = accountFactory.register($scope.email, $scope.firstName, $scope.lastName, $scope.password);
 
             p.then(function () {
-                alert('Registration done, and happy.');
-            })
+                    alert('Registration done, and happy.');
+                })
                 .catch(function () {
                     alert('something went wrong.');
                 });
         };
+
+        $scope.$on('must-login', function() {
+            $location.path('/login');
+        });
+
 
         $log.info('Main controller loaded.');
     }
