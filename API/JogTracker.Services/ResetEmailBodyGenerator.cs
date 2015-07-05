3568cc4427bf02ffa9b1615a742dc125a42b039c
willem <willem@newmedialabs.co.za>
2015-07-05 19:46:20 +0200

    Tweaking reset password API logic.

--- API/JogTracker.Services/ResetEmailBodyGenerator.cs
+++ API/JogTracker.Services/ResetEmailBodyGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace JogTracker.Services
 {
@@ -23,8 +24,13 @@ The Jogging Tracker team";
 
         public string GetEmailBody(string userName, string userId, string token)
         {
-            string url = string.Format("{0}#/choosePassword?uid={1}&token={2}", GlobalConfig.ProdHost, userId, token);
-            return url;
+            string encodedUserId = HttpUtility.UrlEncode(userId);
+            string encodedToken = HttpUtility.UrlEncode(token);
+
+            string url = string.Format("{0}#/choosePassword?uid={1}&token={2}", GlobalConfig.ProdHost, encodedUserId, encodedToken);
+
+            string message = string.Format(ResetPasswordEmailTemplate, url);
+            return message;
         }
     }
 }
